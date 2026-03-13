265274e58486d2a449d1a96be4d7107148d5cf1d
Avril <avrillpzgarxia@gmail.com>
2026-03-13 20:13:10 +0100

    Test a: Constructor vacio implementado

---
+++ src/main/java/com/dam/practicajunit/CirculoTest.java
@@ -0,0 +1,20 @@
+
+ * Autor: avril.lopezgarcia
+ * Data: 13 mar 2026
+ * Hora: 20:12:02
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.dam.practicajunit;
+
+/**
+ *
+ * @author avril.lopezgarcia
+ */
+public class CirculoTest {
+
+}
