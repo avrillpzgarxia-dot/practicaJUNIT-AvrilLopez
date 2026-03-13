3d53061453d4b9710c613dffac848f81ad4611d0
Avril <avrillpzgarxia@gmail.com>
2026-03-13 21:19:37 +0100

    Limpieza manual forzada

--- src/main/java/com/dam/practicajunit/Circulo.java
+++ src/main/java/com/dam/practicajunit/Circulo.java
@@ -8,38 +8,38 @@ package com.dam.practicajunit;
  *
  * @author DANA
  */
-public class Circulo {
-    private double radio;
+public class Circulo { 
+    private double radio; 
 
-    public Circulo() {
-        this.radio = 0.0;
+    public Circulo() {  
+        this.radio = 0.0;  
     }
 
-    public Circulo(double radio) {
-        this.radio = radio;
-    }
+    public Circulo(double radio) {  
+        this.radio = radio; 
+    }  
 
     public double getRadio() {
-        return radio;
-    }
+        return radio; 
+    } 
+ 
+    public void setRadio(double radio) { 
+        this.radio = radio; 
+    } 
+ 
+    public double calcularArea() { 
+        return Math.PI * radio * radio; 
+    } 
 
-    public void setRadio(double radio) {
-        this.radio = radio;
-    }
+    public double calcularPerimetro() { 
+        return 2 * Math.PI * radio; 
+    } 
 
-    public double calcularArea() {
-        return Math.PI * radio * radio;
-    }
+    public String mostrarRadio() { 
+        return "Radio: " + radio; 
+    } 
 
-    public double calcularPerimetro() {
-        return 2 * Math.PI * radio;
+    public static Circulo mayorCirculo(Circulo c1, Circulo c2) { 
+        return (c1.radio >= c2.radio) ? c1 : c2; 
     }
-
-    public String mostrarRadio() {
-        return "Radio: " + radio;
-    }
-
-    public static Circulo mayorCirculo(Circulo c1, Circulo c2) {
-        return (c1.radio >= c2.radio) ? c1 : c2;
-    }
-}
+} 
